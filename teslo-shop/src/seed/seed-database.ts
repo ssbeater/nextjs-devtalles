154095801f3d159b384eb11b9d48d020e44441d5
ssbeater <santiagosuarez.beater@outlook.com>
2024-03-28 16:58:01 -0500

    feat: :sparkles: Prepare Database

---
+++ teslo-shop/src/seed/seed-database.ts
@@ -0,0 +1,53 @@
+import prisma from "../lib/prisma";
+import { initialData } from "./seed";
+import { Categories } from "../interfaces/product.interface";
+
+async function main() {
+  // 1. Erease previous data
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { products, categories } = initialData;
+
+  // 2. Create categories
+  const categoriesData = categories.map((name) => ({ name }));
+  await prisma.category.createMany({
+    data: categoriesData,
+  });
+
+  // 2.1. Get categories map from DB
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  // 3. Create products
+  await Promise.all(
+    products.map(async (product) => {
+      const { images, type, ...rest } = product;
+      const dbProduct = await prisma.product.create({
+        data: { ...rest, categoryId: categoriesMap[type] },
+      });
+
+      // 3.1. Create product images
+      const imagesData = images.map((url) => ({
+        url,
+        productId: dbProduct.id,
+      }));
+
+      await prisma.productImage.createMany({
+        data: imagesData,
+      });
+    })
+  );
+
+  // Finish process
+  console.log("Seed database...");
+}
+
+(() => {
+  if (process.env.NODE_ENV !== "development") return;
+  main();
+})();
